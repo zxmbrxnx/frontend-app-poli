a7706f1c2a2ecb0af8879807da392123b62bc465
Jhonatan Zambrano <jzambrano@nexura.com>
2025-10-07 19:02:42 -0500

    se agrega adaptador de node

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import db from '@astrojs/db';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [db()]
+  integrations: [db()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
